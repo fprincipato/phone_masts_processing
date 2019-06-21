66486eaa13323328adb48ea5877a1b0a3a30e444
Fabrizio Principato <fabrizioprincipato@Fabrizios-MacBook-Pro.local>
2019-06-21 22:18:18 +0100

    fix format for leases_between_dates, add command line arguments and help function to print stats

--- data_process.py
+++ data_process.py
@@ -1,10 +1,13 @@
 import csv
 import datetime
+import argparse
+import pprint
 
 
 class DataProcess(object):
-    def __init__(self, data_file_path):
+    def __init__(self, data_file_path, mode='all'):
         self._data_file_path = data_file_path
+        self._mode = mode
 
     def load_data_file(self):
         with open(self._data_file_path, 'r') as f:
@@ -33,8 +36,36 @@ class DataProcess(object):
         end_date = datetime.datetime(2007,8,1)
         data_filtered = []
         for item in self._data:
-            cur_date = datetime.datetime.strptime(item['Lease Start Date'], '%d-%b-%y')
-            if cur_date >= start_date and cur_date <= end_date:
+            cur_start_date = datetime.datetime.strptime(item['Lease Start Date'], '%d-%b-%y')
+            cur_end_date = datetime.datetime.strptime(item['Lease End Date'], '%d-%b-%y')
+            if cur_start_date >= start_date and cur_start_date <= end_date:
+                item['Lease Start Date'] = datetime.datetime.strftime(cur_start_date, '%d/%m/%Y')
+                item['Lease End Date'] = datetime.datetime.strftime(cur_end_date, '%d/%m/%Y')
                 data_filtered.append(item)
 
         return data_filtered
+
+    def section_one(self):
+        for i in self.sort_data()[:5]:
+            print(i)
+
+    def section_two(self):
+        for i, tot in self.filter_data():
+            print(i)
+
+        print('Total: ', tot)
+
+    def section_three(self):
+        pprint(self.generate_count())
+
+    def section_four(self):
+        for i in self.test_leases_between_dates():
+            print(i)
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Process the csv data and output required stats')
+    parser.add_argument('--mode', dest='mode', default='all', help='This program can be run in different mode. To only print the first 5 items from the csv sorted by Current rent use "section1". To only print a list of masts data for leases of 25 years use "section2". To only print a count of masts per tenants use "section3". To print stats of leases started between 1-Jun-99 and 31-Aug-07 use "section4". To print all stats use "all" (default)')
+    args = parser.parse_args()
+
+    DataProcess('./fixture.csv', mode=args.mode)
+
