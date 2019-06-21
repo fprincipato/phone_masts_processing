73a5c27f0ad76e306947677843bcb1d4fd0f1c4a
Fabrizio Principato <fabrizioprincipato@Fabrizios-MacBook-Pro.local>
2019-06-21 14:36:06 +0100

    Add load data function and test

--- data_process.py
+++ data_process.py
@@ -4,3 +4,8 @@ import csv
 class DataProcess(object):
     def __init__(self, data_file_path):
         self._data_file_path = data_file_path
+
+    def load_data_file(self):
+        with open(self._data_file_path, 'r') as f:
+            reader = csv.DictReader(f)
+            self._data = [item for item in reader]
--- tests.py
+++ tests.py
@@ -11,3 +11,14 @@ class TestDataProcess(unittest.TestCase):
             data_process._data_file_path,
             '/path/to/file'
         )
+    def test_load_data_file(self):
+        data_process = DataProcess('./Test.csv')
+        data_process.load_data_file()
+        expected_keys = [
+            'Property Name', 'Property Address [1]', 'Property  Address [2]',
+            'Property Address [3]', 'Property Address [4]',
+            'Unit Name', 'Tenant Name', 'Lease Start Date',
+            'Lease End Date', 'Lease Years', 'Current Rent',
+        ]
+        self.assertEqual(len(data_process._data), 42)
+        self.assertEqual(list(data_process._data[0].keys()), expected_keys)
